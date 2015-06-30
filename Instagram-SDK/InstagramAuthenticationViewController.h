56a558c82e5da3ce346feb8eafd2f3420f62fad2
Adam Roberts <adam@filterzilla.camera>
2015-06-30 18:17:14 +0100

    Update InstagramAuthenticationViewController.h

--- Instagram-SDK/InstagramAuthenticationViewController.h
+++ Instagram-SDK/InstagramAuthenticationViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Adam Roberts on 30/01/2014.
 //  Copyright (c) 2014 Enigmatic Flare Ltd. All rights reserved.
-//
+// MIT License - http://enigmaticflare.mit-license.org
 
 @import UIKit;
 
